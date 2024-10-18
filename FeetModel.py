35a5f6125967c30227faee73e47203330eb67eef
DarkEmbers <tsharihar@gmail.com>
2024-10-18 20:04:19 +0400

    Implement Linear regression foot prediction

--- FeetModel.py
+++ FeetModel.py
@@ -4,17 +4,9 @@ class KeypointPredictor(nn.Module):
 	def __init__(self):
 		super(KeypointPredictor, self).__init__()
 		self.seq = nn.Sequential(
-			nn.Linear(8, 64),
-			nn.ReLU(),
-			nn.Linear(64, 128),
-			nn.ReLU(),
-			nn.Linear(128, 256),
-			nn.ReLU(),
-			nn.Linear(256, 128),
-			nn.ReLU(),
-			nn.Linear(128, 64),
-			nn.ReLU(),
-			nn.Linear(64, 4)
+			# nn.Linear(8, 8),
+			# nn.ReLU(),
+			nn.Linear(8, 4)
 		)
 
 	def forward(self, x):
