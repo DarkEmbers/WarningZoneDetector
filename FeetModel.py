b89160b2d7fd2f465cb7479fe90c3ed07aed6aaa
DarkEmbers <tsharihar@gmail.com>
2024-12-09 11:15:32 +0400

    Create class and cleanup

--- FeetModel.py
+++ FeetModel.py
@@ -4,8 +4,6 @@ class KeypointPredictor(nn.Module):
 	def __init__(self):
 		super(KeypointPredictor, self).__init__()
 		self.seq = nn.Sequential(
-			# nn.Linear(8, 8),
-			# nn.ReLU(),
 			nn.Linear(8, 4)
 		)
 
